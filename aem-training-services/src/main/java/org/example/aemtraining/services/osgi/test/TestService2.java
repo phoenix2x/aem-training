77ad267c5b70ce80c80cb94a236494a5215b2feb
c-zhavih <ihar.zhavarankau.consultant@consumer.org>
2017-03-30 10:56:25 -0400

    test_feature

--- aem-training-services/src/main/java/org/example/aemtraining/services/osgi/test/TestService2.java
+++ aem-training-services/src/main/java/org/example/aemtraining/services/osgi/test/TestService2.java
@@ -24,6 +24,6 @@ public class TestService2 extends TestService1 implements BindingsValuesProvider
 //            System.out.println("qw");
 //        }
 //        System.out.println("qw");
-        //test1
+        //test1_1
     }
 }
