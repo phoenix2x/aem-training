da5b182ab87dc99bf2ddd25ae87cf10ecb242568
c-zhavih <ihar.zhavarankau.consultant@consumer.org>
2018-01-03 10:23:33 -0500

    ert

---
+++ aem-training-services/src/main/java/org/example/aemtraining/services/osgi/Test.java
@@ -0,0 +1,14 @@
+package org.example.aemtraining.services.osgi;
+
+import java.lang.invoke.MethodHandles;
+
+/**
+ */
+
+public class Test {
+    public static final void main(String...args) {
+//        System.out.println("/cars-a".matches("^/cars-.+"));
+
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+    }
+}
